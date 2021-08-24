600cb1acd2a5be9e6f6c83b902f9f8e8bceebd0c
Piotr <piotr_jasnowski@o2.pl>
2021-08-24 18:54:20 +0200

    reszta plików

--- Apka/Zlecenia/urls.py
+++ Apka/Zlecenia/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from .views import EventListView, HomeView
+from .views import EventListView, HomeView, EventCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('events/', EventListView.as_view(), name='events')
+    path('events/', EventListView.as_view(), name='events'),
+    path('events_create/', EventCreateView.as_view(), name='event_create')
 ]
--- Apka/Zlecenia/views.py
+++ Apka/Zlecenia/views.py
@@ -1,10 +1,17 @@
 from django.views.generic import TemplateView, ListView
 from .models import Event
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-
 class EventListView(ListView):
     template_name = 'event_list.html'
     model = Event
+
+class EventCreateView(CreateView):
+    template_name = 'event_create.html'
+    model = Event
+    fields = ['name', 'description', 'date_from', 'date_to', 'location_name', 'longitude', 'latitude']
+    success_url = reverse_lazy('events')
